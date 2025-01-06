f85dbbbf3e4117a9f1f1432376275a2a57de33e8
gguilherme42 <gguilherme.rosa42@gmail.com>
2025-01-06 10:48:05 -0300

    enable submit button only if url is valid

--- app/renderer.js
+++ app/renderer.js
@@ -1,5 +1,13 @@
-const button = document.querySelector(".alert");
+const linksSection = document.querySelector(".links");
+const errorMessage = document.querySelector(".error-message");
+const newLinkForm = document.querySelector(".new-link-form");
+const newLinkUrl = document.querySelector(".new-link-url");
+const newLinkSubmit= document.querySelector(".new-link-submit");
+const clearStorageButton = document.querySelector(".clear-storage");
 
-button.addEventListener("click", () => {
-    alert(`Path: ${__dirname}`);
+const clearForm = () => {newLinkUrl.value = null;};
+
+
+newLinkUrl.addEventListener("keyup", () => {
+    newLinkSubmit.disabled = !newLinkUrl.validity.valid;
 });
